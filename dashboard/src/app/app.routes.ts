35050f229916302d3415148563b175a4ee571c13
dayanandaeswar <dayanandaeswar@gmail.com>
2023-10-18 19:04:35 +0530

    pushed to repo

---
+++ dashboard/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nrwl/angular/mf';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadChildren: () =>
+        loadRemoteModule('login', './Module').then(
+            (m) => m.RemoteEntryModule
+         ),
+},
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+];
